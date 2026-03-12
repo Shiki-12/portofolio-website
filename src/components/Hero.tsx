0ef64792f8b0dd8c6e65058b064341ca66146cdf
Shiki-12 <uknowndonp@gmail.com>
2026-03-12 22:53:59 +0700

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ import { ArrowRight, MessageSquare } from "lucide-react";
 const roles = [
   "Frontend Developer",
   "System Administrator",
-  "Cybersecurity",
+  "CyberSecurity",
 ];
 
 export default function Hero() {
